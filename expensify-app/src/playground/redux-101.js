0db769a6eb245c5332769045ae1ff939288f0a7f
Morais <ruan.morais@accenture.com>
2018-08-07 13:12:11 -0300

    redux

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -1,7 +1,49 @@
 import { createStore } from 'redux'
 
-const store = createStore((state = { count: 0 }) => {
+const store = createStore((state = { count: 0 }, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1
+            return {
+                count: state.count + incrementBy
+            }
+        case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1
+            return {
+                count: state.count - decrementBy
+            }
+        case 'SET':
+            return {
+                count: action.count
+            }
+        case 'RESET':
+            return {
+                count: 0
+            }
+        default:
+            return state
+    }
     return state;
 });
 
-console.log(store.getState());
+store.subscribe(() => {
+    console.log(store.getState());
+})
+store.dispatch({
+    type: 'INCREMENT',
+    incrementBy: 5
+});
+store.dispatch({
+    type: 'INCREMENT'
+});
+store.dispatch({
+    type: 'RESET'
+});
+store.dispatch({
+    type: 'DECREMENT',
+    decrementBy: 5
+});
+store.dispatch({
+    type: 'SET',
+    count: 101
+});
